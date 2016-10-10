1799b6e9c08bf0e3aa844439644c15a14096e5e8
isidzukuri <axesigon@gmail.com>
2016-10-10 12:46:36 +0300

    category migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010084000) do
+ActiveRecord::Schema.define(version: 20161010094130) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
