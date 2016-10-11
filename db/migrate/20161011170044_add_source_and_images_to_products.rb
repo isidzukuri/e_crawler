c1689fed290a68deb73affbb3b0fe4ab56794685
isidzukuri <axesigon@gmail.com>
2016-10-11 20:15:16 +0300

    migration, source, images added to products

--- db/migrate/20161011170044_add_source_and_images_to_products.rb
+++ db/migrate/20161011170044_add_source_and_images_to_products.rb
@@ -1,4 +1,11 @@
 class AddSourceAndImagesToProducts < ActiveRecord::Migration[5.0]
-  def change
+  def self.up
+    add_column :products, :images, :text, default: ""
+    add_column :products, :source, :string, default: "", null: false
+  end
+
+  def self.down
+    remove_column :products, :images
+    remove_column :products, :source
   end
 end
