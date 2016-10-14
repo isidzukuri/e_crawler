2f732b7c81419edf78bc190eae0e30afc2490401
isidzukuri <axesigon@gmail.com>
2016-10-14 11:27:50 +0300

    source uniqness validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ class Product < ApplicationRecord
   validates :title, presence: true, length: { in: 2..200 }
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 0..2000 }
+  validates :source, uniqueness: true, if: 'source.present?'
 
   def photos
     JSON.parse(images) if images.present?
--- app/repos/items_creator.rb
+++ app/repos/items_creator.rb
@@ -3,9 +3,15 @@ class ItemsCreator
     model = model_name.to_s.classify.constantize
     model.bulk_insert(set_size: 100) do |worker|
       items.each do |attrs|
+        next unless self.valid?(model, attrs)
         attrs.merge!(additonal_params) if additonal_params
         worker.add(attrs)
       end
     end
   end
+
+  def self.valid? model, data
+    model.new(data).valid?
+  end
+
 end
